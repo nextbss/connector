72031f1655acb5afa15ac25345dd149c2c1dc15d
Alexandre Juca <alexandre.juca@nextbss.co.ao>
2020-04-12 21:36:36 +0100

    version 0.0.0

--- lib/connector.rb
+++ lib/connector.rb
@@ -1,7 +1,7 @@
 require 'json'
 require 'faraday'
 
-class Connector
+module Connector
   @base_uri = "https://ec.nextbss.co.ao/api/v1/messages"
   def self.send_sms(message, phone_number, options = {})
     if message == nil
@@ -17,12 +17,13 @@ class Connector
     end
 
     body = { "phone_number" => phone_number, "content" => message }.to_json
-    headers = { "Conent-Type" => "application/json", "X-API-KEY" => options["api_key"] }
+    headers = { "Content-Type" => "application/json", "X-API-KEY" => options["api_key"] }
     resp = Faraday.post(@base_uri, body, headers) 
     if resp.status == 200
 	time = Time.now
         puts "Message Sent at #{time} via EC"
     else
+        puts resp.inspect
 	raise "operation failed: #{resp}" 
     end
   end
